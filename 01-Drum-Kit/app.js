47d8854264e1e11fa825cffd2293f911fb7fcc86
ArchitAdh <architadhikary@gmail.com>
2023-03-17 21:28:18 +0545

    add mouse event and refactored the code

--- 01-Drum-Kit/app.js
+++ 01-Drum-Kit/app.js
@@ -1,29 +1,42 @@
-//TO ADD
-//add mouse key event
-//change bg to drum set and associate key press with the item in image
-// try removing playing class using css variables in css transition and js
-//? a test code
+// TODO:  add mouse key event //COMPLETED
+// TODO:  change bg to drum set and associate key press with the item in image
 
-const playSound = (e) => {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+const playSoundHelper = (audio, key) => {
   if (!audio) return;
   key.classList.add("playing");
   audio.currentTime = 0; // set audio time to beginning before playing again when key pressed
   audio.play();
-
-  //   setTimeout(() => {
-  //     key.classList.remove("playing");
-  //   }, 70);
 };
 
-function removeTransition(e) {
-  console.log(this); // to check what 'this' associates itself  with
-  this.classList.remove("playing");
-}
+const playSoundOnKeyPress = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  playSoundHelper(audio, key);
+};
 
-window.addEventListener("keydown", playSound);
+const playSoundOnClick = (keyCode) => {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+  playSoundHelper(audio, key);
+};
+
+
+// Play sound on keyPress
+window.addEventListener("keydown", playSoundOnKeyPress);
+
+// Play sound  on button click
+const keysUi = document.querySelector(".keys");
+keysUi.addEventListener("click", (e) => {
+  const div = e.target.closest(".key"); // 'closest' method  gets the closest ancestor element that matches a certain selector.
+  const keyCode = div.dataset.key;
+  playSoundOnClick(keyCode);
+});
 
 // remove transition
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+function removeTransition() {
+  // console.log(this); // to check what 'this' associates itself  with
+  this.classList.remove("playing");
+}
