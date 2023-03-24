09d129a43bbdf8f631945849f54bd0a50b8da7e2
ArchitAdh <architadhikary@gmail.com>
2023-03-24 17:37:03 +0545

    js and css clock complete

--- 02-AnalogueClock/app.js
+++ 02-AnalogueClock/app.js
@@ -6,20 +6,19 @@ const setDate = () => {
   const now = new Date();
   const seconds = now.getSeconds();
   console.log("sec", seconds);
-  const secondsDegree = (seconds / 60) * 360 + 90;
+  const secondsDegree = (seconds / 60) * 360;
   //eg: if second is 15s ,15/60 = .25 => .25*360 = 90deg
   // sort of like what we do while making a pie chart
-  // the second part is to account for the transform added in css
   secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 
   const mins = now.getMinutes();
   console.log("min", mins);
-  const minsDegree = (mins / 60) * 360 + 90;
+  const minsDegree = (mins / 60) * 360;
   minHand.style.transform = `rotate(${minsDegree}deg)`;
 
   const hour = now.getHours();
   console.log("hour", hour);
-  const hourDegree = (hour / 12) * 360 + 90;
+  const hourDegree = (hour / 12) * 360;
   hourHand.style.transform = `rotate(${hourDegree}deg)`;
 };
 
